219b16511ac6108a77b19d85cac1f99269cef20a
Alex-Wauters <alex.wauters@gmail.com>
2020-12-17 22:28:41 -0800

    day 8 p 2

--- day 08/halting.go
+++ day 08/halting.go
@@ -14,9 +14,20 @@ type op struct {
 	delta  int
 }
 
+func turnaround(o op) op {
+	if o.action == "nop" {
+		return op{action: "jmp", delta: o.delta}
+	}
+	if o.action == "jmp" {
+		return op{action: "nop", delta: o.delta}
+	}
+	return o
+}
+
 func main() {
 	ops := readInput()
 	fmt.Println(partOne(ops))
+	fmt.Println(partTwo(ops))
 }
 
 func partOne(ops []op) (acc int) {
@@ -25,6 +36,42 @@ func partOne(ops []op) (acc int) {
 	for {
 		if _, isExecuted := executed[index]; isExecuted {
 			return acc
+		}
+		if ops[index].action == "acc" {
+			acc += ops[index].delta
+		}
+		executed[index] = true
+		if ops[index].action == "jmp" {
+			index += ops[index].delta
+		} else {
+			index += 1
+		}
+	}
+}
+
+func partTwo(ops []op) int {
+	for i, operation := range ops {
+		replaced := append(make([]op, 0), ops[:i]...)
+		replaced = append(replaced, turnaround(operation))
+		replaced = append(replaced, ops[i+1:]...)
+		executes, acc := executes(replaced)
+		if executes {
+			return acc
+		}
+	}
+	return 0
+}
+
+func executes(ops []op) (bool, int) {
+	acc := 0
+	executed := make(map[int]bool)
+	index := 0
+	for {
+		if index >= len(ops) {
+			return true, acc
+		}
+		if _, isExecuted := executed[index]; isExecuted {
+			return false, acc
 		}
 		if ops[index].action == "acc" {
 			acc += ops[index].delta
