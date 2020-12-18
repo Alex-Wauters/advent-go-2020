5c477f2a811ae960be2fba1963f609a36007e97a
Alex-Wauters <alex.wauters@gmail.com>
2020-12-17 20:20:34 -0800

    day 6 p2

--- day 06/customs.go
+++ day 06/customs.go
@@ -21,14 +21,40 @@ func (g group) count() int {
 	return len(letters)
 }
 
+func (g group) count2() (count int) {
+	letters := make(map[int32]int)
+	for _, a := range g.answers {
+		for _, c := range a {
+			if _, exists := letters[c]; !exists {
+				letters[c] = 0
+			}
+			letters[c] = letters[c] + 1
+		}
+	}
+	for _, occ := range letters {
+		if occ == len(g.answers) {
+			count++
+		}
+	}
+	return count
+}
+
 func main() {
 	groups := readInput()
 	fmt.Println(partOne(groups))
+	fmt.Println(partTwo(groups))
 }
 
 func partOne(groups []group) (c int) {
 	for _, g := range groups {
 		c += g.count()
+	}
+	return c
+}
+
+func partTwo(groups []group) (c int) {
+	for _, g := range groups {
+		c += g.count2()
 	}
 	return c
 }
