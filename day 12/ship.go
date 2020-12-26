aa2c3cbe4f4545fd6faad20cfa158ef449027c02
Alex-Wauters <alex.wauters@gmail.com>
2020-12-25 17:15:40 -0800

    day 12 part 2

--- day 12/ship.go
+++ day 12/ship.go
@@ -12,6 +12,7 @@ import (
 func main() {
 	lines := readInput()
 	partOne(lines)
+	partTwo(lines)
 }
 
 func partOne(lines []string) {
@@ -58,6 +59,46 @@ func newDir(dir, delta int) int {
 		newDir = 360 + (newDir % 360)
 	}
 	return newDir
+}
+
+func rotateWaypoint(wpX, wpY, degrees int) (int, int) {
+	switch degrees {
+	case 90:
+		return wpY, -wpX
+	case 180:
+		return -wpX, -wpY
+	case 270:
+		return -wpY, wpX
+	case 360:
+		return wpX, wpY
+	}
+	panic(degrees)
+}
+
+func partTwo(lines []string) {
+	x, y := 0, 0
+	wpX, wpY := 10, 1
+	for _, line := range lines {
+		val := toInt(line[1:])
+		switch line[0] {
+		case 'N':
+			wpY += val
+		case 'E':
+			wpX += val
+		case 'S':
+			wpY -= val
+		case 'W':
+			wpX -= val
+		case 'F':
+			x += val * wpX
+			y += val * wpY
+		case 'R':
+			wpX, wpY = rotateWaypoint(wpX, wpY, val)
+		case 'L':
+			wpX, wpY = rotateWaypoint(wpX, wpY, 360-val)
+		}
+	}
+	fmt.Println(math.Abs(float64(x)) + math.Abs(float64(y)))
 }
 
 func readInput() (r []string) {
